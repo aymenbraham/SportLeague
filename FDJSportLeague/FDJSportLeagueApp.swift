de239a00677395e970c978566de3ea009544e6e9
aymenbraham <aymenbraham71@gmail.com>
2025-09-03 12:23:21 +0200

    Initial Commit

---
+++ FDJSportLeague/FDJSportLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJSportLeagueApp.swift
+//  FDJSportLeague
+//
+//  Created by aymen braham on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FDJSportLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FDJSportLeagueTests/FDJSportLeagueTests.swift
@@ -0,0 +1,17 @@
+//
+//  FDJSportLeagueTests.swift
+//  FDJSportLeagueTests
+//
+//  Created by aymen braham on 03/09/2025.
+//
+
+import Testing
+@testable import FDJSportLeague
+
+struct FDJSportLeagueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
